a376e50fc57f0cfdb1fbd5bf39d4035c0c848a2e
aidos-dev <aidos@paygate.kz>
2024-01-11 09:06:15 +0600

    add Spring DI

---
+++ src/main/java/kz/job4j/di/service/SpringDI.java
@@ -0,0 +1,18 @@
+package kz.job4j.di.service;
+
+import kz.job4j.di.model.StartUI;
+import kz.job4j.di.model.Store;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+    }
+}
